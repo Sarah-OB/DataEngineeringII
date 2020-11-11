c2288c29763dd0ad041c34d7b7bf541b5b579f57
Sarah <emmanuelle-sarah.oyono.bisso@efrei.net>
2020-11-11 19:45:30 +0100

    Add test of predict page and updating some lines

--- test_app.py
+++ test_app.py
@@ -19,18 +19,23 @@ class FlaskTests(unittest.TestCase):
         responce = requests.get('http://localhost:5000')
         self.assertEqual(responce.status_code, 200)
 
+    def test_predict_page(self):
+        responce = requests.get('http://localhost:5000')
+        self.assertEqual(responce.status_code, 200)
+
     def test_get_sentiment_positive(self):
         params = {
             'message': 'I am happy',
             'message_user': "analyze_message",
         }
 
+        expected = '<span class="text-center" id="reponse">The sentiment of your text is ' + self.sentiment_positive + '</span>'
         responce = requests.post('http://localhost:5000/predict', data=params)
         self.assertEqual(responce.status_code, 200)
         result = requests.get('http://localhost:5000/predict').text
         reponse_positive = BeautifulSoup(responce.content, 'html.parser')
         positive_rep = reponse_positive.find(class_="text-center")
-        self.assertEqual(str(positive_rep), '<span class="text-center" id="reponse">The sentiment of your text is Positive</span>')
+        self.assertEqual(str(positive_rep), expected)
 
     def test_get_sentiment_negative(self):
         params = {
@@ -38,13 +43,13 @@ class FlaskTests(unittest.TestCase):
             'message_user': "analyze_message",
         }
 
+        expected = '<span class="text-center" id="reponse">The sentiment of your text is ' + self.sentiment_negative + '</span>'
         responce = requests.post('http://localhost:5000/predict', data=params)
         self.assertEqual(responce.status_code, 200)
         result = requests.get('http://localhost:5000/predict').text
         reponse_negative = BeautifulSoup(responce.content, 'html.parser')
         negative_rep = reponse_negative.find(class_="text-center")
-        self.assertEqual(str(negative_rep),
-                         '<span class="text-center" id="reponse">The sentiment of your text is Negative</span>')
+        self.assertEqual(str(negative_rep), expected)
 
     def test_get_sentiment_neutral(self):
         params = {
@@ -52,13 +57,13 @@ class FlaskTests(unittest.TestCase):
             'message_user': "analyze_message",
         }
 
+        expected = '<span class="text-center" id="reponse">The sentiment of your text is ' + self.sentiment_neutral + '</span>'
         responce = requests.post('http://localhost:5000/predict', data=params)
         self.assertEqual(responce.status_code, 200)
         result = requests.get('http://localhost:5000/predict').text
         reponse_neutral = BeautifulSoup(responce.content, 'html.parser')
         neutral_rep = reponse_neutral.find(class_="text-center")
-        self.assertEqual(str(neutral_rep),
-                         '<span class="text-center" id="reponse">The sentiment of your text is ' + self.sentiment_neutral + '</span>')
+        self.assertEqual(str(neutral_rep), expected)
 
 
 if __name__ == '__main__':
